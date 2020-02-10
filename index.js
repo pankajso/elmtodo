c2176fd8522b7518c9d0a43decc8e92961ad13da
Pankaj Sonawane <pankajso@gmail.com>
2020-02-10 20:35:43 +0530

    GunDB data convert to Json

--- index.js
+++ index.js
@@ -10,27 +10,131 @@ const app = Elm.Main.init({
 
 
 
-function load()
+// function load()
+// {
+//
+//
+//     const firebaseConfig = require("./secret/firebase_config.json")
+//       // Initialize Firebase
+//       firebase.initializeApp(firebaseConfig);
+//
+//       const database = firebase.database().ref();
+//       database.on("value", function(snapshot){
+//         // console.log(snapshot.val());
+//         const json = snapshot.val()
+//         console.log("json: ", json)
+//         app.ports.loadFirebaseState.send(json)
+//       });
+//       // listen(database);
+//       console.log("aaaaa");
+// }
+// load();
+
+function loadGun()
 {
+  var gun = Gun(['http://localhost:8765/gun']);
+  // const json = gun.get('tododataa').get("tasklist");
+  // const json2 = JSON.stringify(json);
+  // var j2 = JSON.parse(json);
+  var tododata = {}
+  gun.get('tododataa').once(function (todo, id) {
+    // console.log(todo.newTaskEstimate);
+    // console.log(todo.newTaskName);
+    // console.log(todo.activeTask);
+
+    tododata["newTaskName"] = todo.newTaskName
+    tododata["newTaskEstimate"] = todo.newTaskEstimate
+    tododata["activeTask"] = todo.activeTask
+
+    // console.log(todo.tasklist);
+    // todo.tasklist.map().on(function (task, id) {
+    //    console.log(task.name)
+    //    console.log(task.id)
+    //    console.log(task.estimate)
+    //    console.log(task.actual)
+    // })
+
+    ////////
+    var tlist = {}
+    gun.get('tododataa').get('tasklist').map().on(function (task, id) {
+
+      // console.log(task)
+      // console.log("task id ", id)
+      var data =  { "id" : task.id, "actual" : task.actual, "estimate" : task.estimate, "name" : task.name, "status" : task.status}
+      // tlist[id] = data
+      tlist[id] = data
+      // console.log("data = ", tlist);
+      // if (task) {
+      //
+      // } else {
+      //
+      // }
+    })
+    //////
+    tododata["tasklist"] = tlist
+    // console.log("Todo data =",tododata)
+  })
 
 
-    const firebaseConfig = require("./secret/firebase_config.json")
-      // Initialize Firebase
-      firebase.initializeApp(firebaseConfig);
+  // const json = JSON.parse(tododata);
+  // const json = tododata;
+  const json = JSON.stringify(tododata);
+// var data =
+// {
+//   "newTaskName":"",
+//   "newTaskEstimate":30,
+//   "activeTask":0,
+//   "tasklist":
+//     {"Lwe":
+//       {
+//         "id": 1,
+//         "actual":0,
+//         "estimate":30,
+//         "name":"aa",
+//         "status":"Pause"
+//       },
+//       "Lwf":
+//       {
+//         "id": 2,
+//         "actual":0,
+//         "estimate":30,
+//         "name":"bb",
+//         "status":"Pause"
+//       },
+//       "Lwg":
+//       {
+//         "id": 3,
+//         "actual":0,
+//         "estimate":30,
+//         "name":"cc",
+//         "status":"Pause"
+//       },
+//       "Lwh":
+//       {
+//         "id": 4,
+//         "actual":0,
+//         "estimate":30,
+//         "name":"dd",
+//         "status":"Pause"
+//       },
+//       "Lwi":
+//       {
+//         "id": 5,
+//         "actual":0,
+//         "estimate":30,
+//         "name":"ee",
+//         "status":"Pause"
+//       }
+//     }
+//   }
 
-      const database = firebase.database().ref();
-      database.on("value", function(snapshot){
-        // console.log(snapshot.val());
-        const json = snapshot.val()
-        console.log("json: ", json)
-        app.ports.loadFirebaseState.send(json)
-      });
-      // listen(database);
-      console.log("aaaaa");
+
+  console.log("json: ", json);
+  // console.log("json: ", j2);
+  app.ports.loadFirebaseState.send(json);
 }
-load();
 
-
+loadGun();
 app.ports.sendNewTaskState.subscribe(str => {
         writeNewTask(str);
     })
