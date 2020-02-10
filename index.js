7887e8ccef9472218022817a79296605e8d354ab
Girish Sonawane <girish.sonawane@gmail.com>
2020-02-10 20:59:47 +0530

    cleanup js

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-import { Elm } from './Main.elm'
+import {
+    Elm
+} from './Main.elm'
 import './main.css'
 import firebase from "firebase"
 import gun from "gun"
 
 
 const app = Elm.Main.init({
-  node: document.getElementById('main')
+    node: document.getElementById('main')
 });
 
 
@@ -30,114 +32,121 @@ const app = Elm.Main.init({
 // }
 // load();
 
-function loadGun()
-{
-  var gun = Gun(['http://localhost:8765/gun']);
-  // const json = gun.get('tododataa').get("tasklist");
-  // const json2 = JSON.stringify(json);
-  // var j2 = JSON.parse(json);
-  var tododata = {}
-  gun.get('tododataa').once(function (todo, id) {
-    // console.log(todo.newTaskEstimate);
-    // console.log(todo.newTaskName);
-    // console.log(todo.activeTask);
+function loadGun() {
+    var gun = Gun(['http://localhost:8765/gun']);
+    // const json = gun.get('tododataa').get("tasklist");
+    // const json2 = JSON.stringify(json);
+    // var j2 = JSON.parse(json);
+    var tododata = {}
+    gun.get('tododataa').once(function(todo, id) {
+        // console.log(todo.newTaskEstimate);
+        // console.log(todo.newTaskName);
+        // console.log(todo.activeTask);
 
-    tododata["newTaskName"] = todo.newTaskName
-    tododata["newTaskEstimate"] = todo.newTaskEstimate
-    tododata["activeTask"] = todo.activeTask
+        tododata["newTaskName"] = todo.newTaskName
+        tododata["newTaskEstimate"] = todo.newTaskEstimate
+        tododata["activeTask"] = todo.activeTask
 
-    // console.log(todo.tasklist);
-    // todo.tasklist.map().on(function (task, id) {
-    //    console.log(task.name)
-    //    console.log(task.id)
-    //    console.log(task.estimate)
-    //    console.log(task.actual)
-    // })
+        // console.log(todo.tasklist);
+        // todo.tasklist.map().on(function (task, id) {
+        //    console.log(task.name)
+        //    console.log(task.id)
+        //    console.log(task.estimate)
+        //    console.log(task.actual)
+        // })
 
-    ////////
-    var tlist = {}
-    gun.get('tododataa').get('tasklist').map().on(function (task, id) {
+        ////////
+        var tlist = {}
+        gun.get('tododataa').get('tasklist').map().on(function(task, id) {
 
-      // console.log(task)
-      // console.log("task id ", id)
-      var data =  { "id" : task.id, "actual" : task.actual, "estimate" : task.estimate, "name" : task.name, "status" : task.status}
-      // tlist[id] = data
-      tlist[id] = data
-      // console.log("data = ", tlist);
-      // if (task) {
-      //
-      // } else {
-      //
-      // }
+            // console.log(task)
+            // console.log("task id ", id)
+            var data = {
+                "id": task.id,
+                "actual": task.actual,
+                "estimate": task.estimate,
+                "name": task.name,
+                "status": task.status
+            }
+            // tlist[id] = data
+            tlist[id] = data
+            tododata["tasklist"] = tlist
+            const json = JSON.stringify(tododata);
+
+            console.log("json: ", json);
+
+            // console.log("data = ", tlist);
+            // if (task) {
+            //
+            // } else {
+            //
+            // }
+        })
+        //////
+        // console.log("Todo data =",tododata)
     })
-    //////
-    tododata["tasklist"] = tlist
-    // console.log("Todo data =",tododata)
-  })
 
 
-  // const json = JSON.parse(tododata);
-  // const json = tododata;
-  const json = JSON.stringify(tododata);
-// var data =
-// {
-//   "newTaskName":"",
-//   "newTaskEstimate":30,
-//   "activeTask":0,
-//   "tasklist":
-//     {"Lwe":
-//       {
-//         "id": 1,
-//         "actual":0,
-//         "estimate":30,
-//         "name":"aa",
-//         "status":"Pause"
-//       },
-//       "Lwf":
-//       {
-//         "id": 2,
-//         "actual":0,
-//         "estimate":30,
-//         "name":"bb",
-//         "status":"Pause"
-//       },
-//       "Lwg":
-//       {
-//         "id": 3,
-//         "actual":0,
-//         "estimate":30,
-//         "name":"cc",
-//         "status":"Pause"
-//       },
-//       "Lwh":
-//       {
-//         "id": 4,
-//         "actual":0,
-//         "estimate":30,
-//         "name":"dd",
-//         "status":"Pause"
-//       },
-//       "Lwi":
-//       {
-//         "id": 5,
-//         "actual":0,
-//         "estimate":30,
-//         "name":"ee",
-//         "status":"Pause"
-//       }
-//     }
-//   }
+    // const json = JSON.parse(tododata);
+    // const json = tododata;
+    // var data =
+    // {
+    //   "newTaskName":"",
+    //   "newTaskEstimate":30,
+    //   "activeTask":0,
+    //   "tasklist":
+    //     {"Lwe":
+    //       {
+    //         "id": 1,
+    //         "actual":0,
+    //         "estimate":30,
+    //         "name":"aa",
+    //         "status":"Pause"
+    //       },
+    //       "Lwf":
+    //       {
+    //         "id": 2,
+    //         "actual":0,
+    //         "estimate":30,
+    //         "name":"bb",
+    //         "status":"Pause"
+    //       },
+    //       "Lwg":
+    //       {
+    //         "id": 3,
+    //         "actual":0,
+    //         "estimate":30,
+    //         "name":"cc",
+    //         "status":"Pause"
+    //       },
+    //       "Lwh":
+    //       {
+    //         "id": 4,
+    //         "actual":0,
+    //         "estimate":30,
+    //         "name":"dd",
+    //         "status":"Pause"
+    //       },
+    //       "Lwi":
+    //       {
+    //         "id": 5,
+    //         "actual":0,
+    //         "estimate":30,
+    //         "name":"ee",
+    //         "status":"Pause"
+    //       }
+    //     }
+    //   }
 
 
-  console.log("json: ", json);
-  // console.log("json: ", j2);
-  app.ports.loadFirebaseState.send(json);
+    // console.log("json: ", j2);
+    app.ports.loadFirebaseState.send(json);
 }
 
 loadGun();
 app.ports.sendNewTaskState.subscribe(str => {
-        writeNewTask(str);
-    })
+    writeNewTask(str);
+})
 
 function writeNewTask(task) {
     // A Task entry.
